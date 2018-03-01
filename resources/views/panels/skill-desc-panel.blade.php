39f84c72fa263cb0a03f7a0008c8c855f6c89f00
Mayank Sehgal <mayank@mayanksehgal.com>
2018-03-01 16:37:32 +0530

    Fixed 'refer to' bug

--- resources/views/panels/skill-desc-panel.blade.php
+++ resources/views/panels/skill-desc-panel.blade.php
@@ -2,14 +2,15 @@
     <h3><?= $param['skill']->getLocalName(); ?></h3>
 
     <hr>
-
     <div id="node-description"></div>
-
     <script src="js/jquery.wikiblurb.js" type="text/javascript"></script>
 
     <script type="text/javascript">
         $(document).ready(function(){
+            getDesc();
+        });
 
+        function getDesc() {
             $('#node-description').wikiblurb({
                 wikiURL: "http://en.wikipedia.org/",
                 apiPath: "w",
@@ -19,8 +20,19 @@
                 type: "blurb"
             });
 
-        });
+            setTimeout(function() {
+                var wikisubs = document.getElementById("node-description").innerText;
+                var sliced = wikisubs.slice(-11);
+                var check = "refer to:";
+                sliced = sliced.trim();
 
+                console.log(sliced)
+
+                if(sliced === check) {
+                    document.getElementById("node-description").innerHTML = "There is no description available for this node.";
+                }
+            }, 1000);
+        }
     </script>
 
     <div class="panel-bottom">
